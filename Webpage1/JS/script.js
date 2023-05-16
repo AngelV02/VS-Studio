1207c4edc85a592480846aafc2dd90c3ccf95fec
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-05-16 11:19:40 -0700

    added code

--- Webpage1/JS/script.js
+++ Webpage1/JS/script.js
@@ -1,1 +1,12 @@
+var musicVideo = document.getElementById("music-video");
+
+function animateVideo() {
+    musicVideo.style.transform = "rotate(360deg)";
+    musicVideo.style.transition = "transform 2s ease-in-out";
+}
+
+// Call the animateVideo function after the page has loaded
+window.onload = function () {
+    animateVideo();
+};
 
